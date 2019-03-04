b0256079829f0361d340d8766d8306ddaf9dac15
“yhj <yhj@123.com>
2019-03-04 15:40:09 +0800

    first commit

---
+++ microservicecloud-api/src/main/java/com/yhj/springcloud/microservicecloud/api/entity/Dept.java
@@ -0,0 +1,18 @@
+package com.yhj.springcloud.microservicecloud.api.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Accessors(chain = true)
+public class Dept implements Serializable {
+    private Long deptno;
+    private String dname;
+    private String db_source;
+}
