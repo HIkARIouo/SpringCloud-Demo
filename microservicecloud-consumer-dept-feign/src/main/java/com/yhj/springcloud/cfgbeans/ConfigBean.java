d7666268b041f068f6c7a5f5e3adf93d150d0d28
“yhj <yhj@123.com>
2019-03-05 08:41:27 +0800

    feign

--- microservicecloud-consumer-dept-feign/src/main/java/com/yhj/springcloud/cfgbeans/ConfigBean.java
+++ microservicecloud-consumer-dept-feign/src/main/java/com/yhj/springcloud/cfgbeans/ConfigBean.java
@@ -1,5 +1,7 @@
 package com.yhj.springcloud.cfgbeans;
 
+import com.netflix.loadbalancer.IRule;
+import com.netflix.loadbalancer.RetryRule;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -8,10 +10,18 @@ import org.springframework.web.client.RestTemplate;
 
 @Configuration
 public class ConfigBean {
+    @Bean
+    @LoadBalanced//Spring Cloud Ribbon是基于Netflix Ribbon实现的一套客户端       负载均衡的工具。
+    public RestTemplate getRestTemplate()
+    {
+        return new RestTemplate();
+    }
 
     @Bean
-    @LoadBalanced
-    public RestTemplate restTemplate(){
-        return  new RestTemplate();
+    public IRule myRule()
+    {
+        //return new RoundRobinRule();
+        //return new RandomRule();//达到的目的，用我们重新选择的随机算法替代默认的轮询。
+        return new RetryRule();
     }
 }
